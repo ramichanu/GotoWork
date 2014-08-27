6e5cfe713c8b176186f1d011b0b146bc3e717d16
Rasen <ramirezsa89@gmail.com>
2014-08-27 08:19:53 +0200

    Door travel system

---
+++ Assets/scripts/building/Door.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Door : MonoBehaviour {
+
+	List<GameObject> connectedDoors;
+	Animator doorAnimator;
+
+	// Los dos siguientes atributos no seran necesarios
+	GameObject character;
+	public string connectedDoorId = "door2";
+
+	// Use this for initialization
+	void Start () {
+	
+
+		doorAnimator = this.GetComponent<Animator> ();
+
+		connectedDoors = new List<GameObject>();
+		GameObject door = GameObject.Find(connectedDoorId) ;
+		connectedDoors.Add(door);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AnimatorStateInfo stateInfo = doorAnimator.GetCurrentAnimatorStateInfo(0);
+	
+		if(stateInfo.nameHash == Animator.StringToHash("Base Layer.OpenDoor"))
+		{
+
+			GameObject connectedDoor = (GameObject)connectedDoors[0];
+			Vector2 connectedDoorPosition = new Vector2(connectedDoor.transform.position.x, connectedDoor.transform.position.y-(float)0.286);
+
+			character.transform.position = connectedDoorPosition;
+
+
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		character = other.gameObject;
+		if(character.tag == "character" && Input.GetKeyUp("up"))
+		{  
+			doorAnimator.SetBool ("openDoor", true);
+
+		}
+	}
+}
---
+++ Assets/scripts/character/character2dMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character2dMovement : MonoBehaviour {
+
+	Vector2 movementSpeed = new Vector2 (5, 0);
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			rigidbody2D.MovePosition(rigidbody2D.position + movementSpeed * Time.deltaTime);
+			
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			rigidbody2D.MovePosition(rigidbody2D.position - movementSpeed * Time.deltaTime);
+			
+		}
+	}
+
+}
