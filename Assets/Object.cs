89f28788c875fa012571301d4d1191aa6e76238e
Jibun <jibun@hotmail.com>
2014-08-31 11:57:03 +0200

    Avanze por Ivan

--- Assets/Object.cs
+++ Assets/Object.cs
@@ -1,35 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Object : MonoBehaviour {
 
 	public string characterAnimationUse = null;
 	public string characterAnimationUnuse = null;
 	public string objectAnimation = null;
-	//bool charFront = false;
 	bool used = false;
-	//GameObject character = null;
+	GameObject usedBy = null;
 
 	bool isColliding(GameObject character){
 		return renderer.bounds.Intersects(character.renderer.bounds);
 	}
 
 	void use(GameObject character){
-		if(used) return;
-		if(characterAnimationUse != null && characterAnimationUse != ""){
-			character.GetComponent<Animator> ().SetTrigger(characterAnimationUse);
-		}
-		if(objectAnimation != null && objectAnimation != ""){
-			this.GetComponent<Animator> ().SetTrigger(objectAnimation);
-		}
-		used = true;
-	}
+		if(used && usedBy != character) return;
+		if(!isColliding(character)) return;
 
-	void unuse(GameObject character){
-		if(characterAnimationUnuse != null && characterAnimationUnuse != ""){
-			character.GetComponent<Animator> ().SetTrigger(characterAnimationUnuse);
+		if(!used){
+			if(characterAnimationUse != null && characterAnimationUse != ""){
+				character.GetComponent<Animator> ().SetTrigger(characterAnimationUse);
+			}
+			if(objectAnimation != null && objectAnimation != ""){
+				this.GetComponent<Animator> ().SetTrigger(objectAnimation);
+			}
+			used = true;
+			usedBy = character;
+
+			this.SendMessage ("executeEffect", character, SendMessageOptions.DontRequireReceiver);
+		}else{
+			if(characterAnimationUnuse != null && characterAnimationUnuse != ""){
+				character.GetComponent<Animator> ().SetTrigger(characterAnimationUnuse);
+			}
+			used = false;
+			usedBy = null;
 		}
-		used = false;
 	}
 
 	// Use this for initialization
@@ -40,25 +46,6 @@ public class Object : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		/*if (Input.GetKeyUp ("up") && charFront) {
-			doorAnimator.SetBool ("openDoor", true);
-		} */
-	}
-
-	/*void OnTriggerExit2D(Collider2D other) {
-		character = other.gameObject;
-		if (character.tag == "character") { 
-			charFront = false;
-			character = null;
-		}
 	}
 	
-	void OnTriggerEnter2D(Collider2D other) {
-		
-		character = other.gameObject;
-		if (character.tag == "character") {  
-			charFront = true;
-			
-		} 
-	}*/
 }
---
+++ Assets/scripts/ChangeColorEffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorEffect : MonoBehaviour {
+
+	/*void executeEffect(GameObject character){
+		this.GetComponent<SpriteRenderer>().color = Color.red;
+	}*/
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
